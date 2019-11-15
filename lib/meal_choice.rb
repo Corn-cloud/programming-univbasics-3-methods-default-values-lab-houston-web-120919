f05e054650ff8c766def3cc4a4be763570217685
CORNELIUS OMOWAIYE <comowaiye@gmail.com>
2019-11-15 00:35:09 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,8 +3,8 @@
 
 def meal_choice(veg1, veg2, protein= "meat")
   puts "What a nutritious meal!"
-return  "A plate of #{protein} with #{veg1} and #{veg2}."
-puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts return  "A plate of #{protein} with #{veg1} and #{veg2}."
+
 
 end 
 
